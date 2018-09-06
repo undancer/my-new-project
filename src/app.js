61753f9ab0fac805dfbcb0eb3caddb65addbd01c
undancer <undancer@gmail.com>
2018-09-06 19:15:40 +0800

    update .idea

---
+++ src/app.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const React = require("react");
+class App extends React.Component {
+    render() {
+        return (React.createElement("div", null,
+            React.createElement("h2", null, "Welcome to React with Typescript!")));
+    }
+}
+exports.App = App;
+//# sourceMappingURL=app.js.map
