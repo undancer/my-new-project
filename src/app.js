0f61b3905f6ef29dae540888179c1a5eec5b7273
undancer <undancer@gmail.com>
2018-09-06 21:23:54 +0800

    添加material-ui

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const React = require("react");
+const core_1 = require("@material-ui/core");
 class App extends React.Component {
     render() {
         return (React.createElement("div", null,
-            React.createElement("h2", null, "Welcome to React with Typescript!")));
+            React.createElement("h2", null, "Welcome to React with Typescript!"),
+            React.createElement(core_1.Button, { variant: 'contained', color: 'primary' }, "Hello World")));
     }
 }
 exports.default = App;
--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
+import {Button} from '@material-ui/core';
 
 export default class App extends React.Component<{}, {}> {
     render() {
         return (
             <div>
                 <h2>Welcome to React with Typescript!</h2>
+                <Button variant='contained' color='primary'>
+                    Hello World
+                </Button>
             </div>
         );
     }
